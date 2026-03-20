5b6021a8794046b1933afca1b29dc2343c2bad12
Arav Ghiya <arav.ghiya@somaiya.edu>
2026-03-20 11:40:18 +0530

    updated website

--- frontend/src/pages/Rooms.jsx
+++ frontend/src/pages/Rooms.jsx
@@ -13,19 +13,19 @@ return(
 <div className="roomCard">
 <img src="https://images.unsplash.com/photo-1566073771259-6a8506099945"/>
 <h3>Deluxe Suite</h3>
-<p>₹12,000 / night</p>
+<p>₹4,000 / night</p>
 </div>
 
 <div className="roomCard">
 <img src="https://images.unsplash.com/photo-1590490360182-c33d57733427"/>
 <h3>Executive Suite</h3>
-<p>₹18,000 / night</p>
+<p>₹8,000 / night</p>
 </div>
 
 <div className="roomCard">
 <img src="https://images.unsplash.com/photo-1582719508461-905c673771fd"/>
 <h3>Presidential Suite</h3>
-<p>₹30,000 / night</p>
+<p>₹12,000 / night</p>
 </div>
 
 </div>
